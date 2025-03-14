7baf664635cdba301f18cc3ed544071fc55ac486
mohamedelsayed0001 <mohamedelsayyed234@gmail.com>
2025-03-14 15:43:13 +0200

    constructor update

---
+++ src/main/java/com/example/Sort_Array.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.util.Scanner;
+
+class Sort_Array {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Constructor constructor =new Constructor();
+        
+        int [] array = constructor.read_input();
+        System.out.println("chose method :");
+        System.out.println("1- O(n^2) ");
+        System.out.println("2- O(n log n)");
+        System.out.println("3- O(n)");
+        int method = scanner.nextInt();
+        if(method == 2){
+            SortingStrategy sort = new QuickSort();
+            long startTime = System.nanoTime();
+            sort.sort(array);
+            long endTime = System.nanoTime();
+            System.out.println("Execution Time for QuickSort : " + (endTime - startTime) / 1_000_000.0 + " ms");
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.err.print(array[i]+", ");
+        }
+
+    }
+    
+}
